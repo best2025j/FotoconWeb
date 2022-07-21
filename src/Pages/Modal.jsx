023c8bdb0056b4c2fc4b2a854d405be4acf77f42
best <gt@best.com>
2022-07-21 12:17:21 +0100

    error

--- src/Pages/Modal.jsx
+++ src/Pages/Modal.jsx
@@ -11,12 +11,11 @@ const Backdrop = (props) => {
 // Modal
 const ModalOverlay = (props) => {
   return (
-    <div className="fixed lg:top-[16vh] top-[16vh] lg:w-[30rem] md:w-[30rem] sm:w-[24rem] w-[20rem] md:left-[8rem] lg:left-[26rem] left-[26px] shadow-2xl bg-white p-4 z-10 rounded-2xl">
+    <div className="fixed lg:top-[24vh] top-[20vh] lg:w-[30rem] md:w-[30rem] sm:w-[24rem] w-[20rem] md:left-[12rem] sm:left-[8rem] lg:left-[26rem] left-[26px] shadow-2xl bg-white p-4 z-10 rounded-2xl">
       <div className="text-black py-4">{props.children}</div>
     </div>
   );
 };
-
 const portalElement = document.getElementById("overlays");
 
 // modal functions
